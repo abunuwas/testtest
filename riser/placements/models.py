59beea63f82c653ebd79bd37d6aefb80bf4ddf83
Sam Ghadri <sam@Sams-MacBook-Pro.local>
2017-10-01 16:45:13 +0100

    Add Placement models

--- riser/placements/models.py
+++ riser/placements/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField()
+    username = models.CharField(max_length=120)
+
+
+class Company(models.Model):
+    registration = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=200)
+
+
+class Placement(models.Model):
+    DECLINED = 'Declined'
+    APPROVED = 'Approved'
+    PENDING = 'Pending'
+    NOT_SUBMITTED = 'NOT_Submitted'
+
+    reference = models.AutoField(primary_key=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    status = models.CharField(max_length=20,
+                              choices=(
+                                  (APPROVED, 'Approved'),
+                                  (PENDING, 'Pending'),
+                                  (NOT_SUBMITTED, 'Not Submitted'),
+                                  (DECLINED, 'Declined'),
+                              ),
+                              default=NOT_SUBMITTED)
+    candidate = models.ForeignKey(Candidate)
+    company = models.ForeignKey(Company)
